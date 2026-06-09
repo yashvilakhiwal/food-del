b6f4a5ced16d65f1ecba8e735a4a8cc70c96e5d7
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-06-09 17:56:23 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
 
-    const url="http://localhost:4000";
+    const url="https://food-del-backend-gl5o.onrender.com";
     const [token,setToken]= useState("");
     const [food_list,setFoodList]=useState([])
     const addToCart = async  (itemId) => {
